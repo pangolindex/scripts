b909d9a3f61d5a855582165f6f227740bbd7f941
Brandon <brandon@pangolin.exchange>
2022-02-28 13:30:00 -0500

    Use live PGL values
    Significantly improve buyback bot

--- src/fee_collector/harvest.js
+++ src/fee_collector/harvest.js
@@ -3,6 +3,7 @@ const CONFIG = require('../../config/config');
 const ABI = require('../../config/abi.json');
 const ADDRESS = require('../../config/address.json');
 
+const axios = require('axios');
 const Web3 = require('web3');
 const web3 = new Web3(new Web3.providers.HttpProvider('https://api.avax.network/ext/bc/C/rpc'));
 web3.eth.accounts.wallet.add(CONFIG.WALLET.KEY);
@@ -12,10 +13,7 @@ let endingAvax;
 
 // Change These Variables
 // --------------------------------------------------
-// ZapperFi overview of swap fees available: https://zapper.fi/account/0xa622f4334b40d879dd2a5dccf9eeeb66f7592cd0/protocols/avalanche/pangolin
-const pglAddresses = [
-    '0x0000000000000000000000000000000000000000',
-];
+const minimumHarvestBalanceUSD = 1000;
 // --------------------------------------------------
 
 
@@ -24,16 +22,72 @@ const pglAddresses = [
     console.log(`Starting AVAX: ${startingAvax / (10 ** 18)}`);
 
     const feeCollectorContract = new web3.eth.Contract(ABI.FEE_COLLECTOR, ADDRESS.FEE_COLLECTOR);
+    const harvestIncentive = parseInt(await feeCollectorContract.methods.harvestIncentive().call());
+    const feeDenominator = parseInt(await feeCollectorContract.methods.FEE_DENOMINATOR().call());
+
+    const { data: { data: { liquidityPositions } } } = await axios({
+        url: 'https://api.thegraph.com/subgraphs/name/pangolindex/exchange',
+        method: 'post',
+        data: {
+            query: `query {
+                liquidityPositions(
+                    first: 1000
+                    where: { user: "${ADDRESS.FEE_COLLECTOR.toLowerCase()}" }
+                ) {
+                    id
+                    pair {
+                        id
+                        reserveUSD
+                        reserveETH
+                        totalSupply
+                    }
+                    liquidityTokenBalance
+                }
+            }`
+        }
+    });
+
+    const positions = [];
+
+    for (const position of liquidityPositions) {
+        if (parseFloat(position.liquidityTokenBalance) === 0) continue;
+        if (parseFloat(position.pair.totalSupply) === 0) continue;
+        if (parseFloat(position.pair.reserveUSD) === 0) continue;
+
+        const percentOwnership = parseFloat(position.liquidityTokenBalance) / parseFloat(position.pair.totalSupply);
+
+        positions.push({
+            pgl: position.pair.id,
+            valueUSD: percentOwnership * parseFloat(position.pair.reserveUSD),
+            valueAVAX: percentOwnership * parseFloat(position.pair.reserveETH),
+        });
+    }
+
+    const acceptedPositions = positions
+        .filter(p => p.valueUSD >= minimumHarvestBalanceUSD)
+        .sort((a,b) => a.valueUSD > b.valueUSD ? -1 : 1);
+
+    const totalValueUSD = acceptedPositions.reduce((sum, pos) => sum += pos.valueUSD, 0);
+    const totalValueAVAX = acceptedPositions.reduce((sum, pos) => sum += pos.valueAVAX, 0);
+    const harvestIncentiveValueUSD = totalValueUSD * (harvestIncentive / feeDenominator);
+    const harvestIncentiveValueAVAX = totalValueAVAX * (harvestIncentive / feeDenominator);
 
     const tx = feeCollectorContract.methods.harvest(
-        pglAddresses,
+        acceptedPositions.map(p => p.pgl),
         false, // claimMiniChef
     );
     const gas = await tx.estimateGas({ from: CONFIG.WALLET.ADDRESS });
     const baseGasPrice = await web3.eth.getGasPrice();
     const expectedGasPrice = parseInt(baseGasPrice) + parseInt(web3.utils.toWei('2', 'nano'));
+    const expectedGasAVAX = gas * expectedGasPrice / (10 ** 18);
 
-    console.log(`Estimated gas cost of ~${(gas * expectedGasPrice / (10 ** 18)).toLocaleString(undefined, {minimumFractionDigits: 3})} AVAX`);
+    console.log(`Estimated incentive of $${harvestIncentiveValueUSD.toLocaleString(undefined, {maximumFractionDigits: 2})} (${harvestIncentiveValueAVAX.toLocaleString(undefined, {maximumFractionDigits: 3})} AVAX)`);
+    console.log(`Estimated gas cost of ~${expectedGasAVAX.toLocaleString(undefined, {minimumFractionDigits: 3})} AVAX`);
+
+    if (harvestIncentiveValueAVAX < expectedGasAVAX) {
+        console.log(`Insufficient harvest incentive detected: aborting transaction submission`);
+        return;
+    }
 
     console.log('Sending harvest() ...');
     const receipt = await tx.send({
