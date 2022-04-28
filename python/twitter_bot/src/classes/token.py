38a111d815260c01dfcdee5d1124028e779ddcbe
Pedro <pedroantonioppms@gmail.com>
2022-04-28 11:09:21 -0400

    Added _logo property em token class

--- python/twitter_bot/src/classes/token.py
+++ python/twitter_bot/src/classes/token.py
@@ -1,12 +1,16 @@
 from eth_utils import to_checksum_address
 
 class Token:
-    def __init__(self, address: str, name: str = None, symbol: str = None):
+    def __init__(self, address: str, name: str = None, symbol: str = None, logo: str = None):
         self.name = name
         self.symbol = symbol
         self.address = to_checksum_address(address)
+        self._logo = logo
     
-    def logo(self, size: int | None):
+    def logo(self, size: int | None = None):
+        if self._logo:
+            return self._logo
+
         url = f"https://raw.githubusercontent.com/pangolindex/tokens/main/assets/{self.address}/"
         url += f"logo_{size}.png" if size else "logo.png"
         return url
