23d2c71f841b993f754a5968ca0ef9c7ef04ca69
Brandon <brandon@pangolin.exchange>
2022-07-11 16:23:05 -0400

    Auto buyback scheduling & checks

--- src/core/constants.js
+++ src/core/constants.js
@@ -4,6 +4,11 @@ const constants = {
     EOA: 'EOA',
 
     MAX_UINT256: '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff',
+
+    SECOND: 1000,
+    MINUTE: 60 * 1000,
+    HOUR: 60 * 60 * 1000,
+    DAY: 24 * 60 * 60 * 1000,
 };
 
 module.exports = constants;
