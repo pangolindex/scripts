ec5264ea4192f62d011136ae45647cca3526c9b3
Brandon <brandon@pangolin.exchange>
2022-08-23 11:33:25 -0400

    Activate partner and modify total fee

---
+++ src/DaaS/modifyTotalFee.js
@@ -0,0 +1,51 @@
+// Helper modules to provide common or secret values
+const CONFIG = require('../../config/config');
+const ADDRESS = require('../../config/address.json');
+const ABI = require('../../config/abi.json');
+
+const Web3 = require('web3');
+const web3 = new Web3(new Web3.providers.HttpProvider('https://api.avax.network/ext/bc/C/rpc'));
+web3.eth.accounts.wallet.add(CONFIG.WALLET.KEY);
+let startingAvax;
+let endingAvax;
+
+
+// Change These Variables
+// --------------------------------------------------
+const partner = '0x0000000000000000000000000000000000000000';
+const feeTotal = 100;
+const router = ADDRESS.DAAS_ROUTER_MAINNET;
+// --------------------------------------------------
+
+
+(async () => {
+    startingAvax = await web3.eth.getBalance(CONFIG.WALLET.ADDRESS);
+
+    const routerDaaS = new web3.eth.Contract(ABI.ROUTER_DAAS, router);
+
+    const tx = routerDaaS.methods.modifyTotalFee(partner, feeTotal.toString());
+
+    const gas = await tx.estimateGas({ from: CONFIG.WALLET.ADDRESS });
+    const baseGasPrice = await web3.eth.getGasPrice();
+
+    console.log('Sending tx ...');
+    const receipt = await tx.send({
+        from: CONFIG.WALLET.ADDRESS,
+        gas: gas,
+        maxFeePerGas: baseGasPrice * 2,
+        maxPriorityFeePerGas: web3.utils.toWei('1', 'nano'),
+    });
+
+    if (!receipt?.status) {
+        console.log(receipt);
+        process.exit(1);
+    } else {
+        console.log(`Transaction hash: ${receipt.transactionHash}`);
+    }
+})()
+    .catch(console.error)
+    .finally(async () => {
+        endingAvax = await web3.eth.getBalance(CONFIG.WALLET.ADDRESS);
+        console.log(`AVAX spent: ${(startingAvax - endingAvax) / (10 ** 18)}`);
+        process.exit(0);
+    });
