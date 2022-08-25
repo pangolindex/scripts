88846505f0c2c54490063a15d9309188ac4ba3e4
Brandon <brandon@pangolin.exchange>
2022-08-25 17:24:28 -0400

    Multichain Configurable Bots

---
+++ src/bots/ecosystem/example.config.js
@@ -0,0 +1,46 @@
+/**
+ * Ecosystem configuration used by pm2 (https://pm2.io/)
+ */
+const config = [
+  {
+    name: 'vester',
+    script: './vesterBot.js',
+    env: {
+      WALLET: '',
+      KEY: '',
+      RPC: 'https://api.avax.network/ext/bc/C/rpc',
+      TREASURY_VESTER: '0x6747AC215dAFfeE03a42F49FebB6ab448E12acEe',
+      TREASURY_VESTER_PROXY: '0x503C4e38c80B1D17e2c653E142770CeA060a8bB7',
+    },
+  },
+  {
+    name: 'harvester',
+    script: './harvesterBot.js',
+    env: {
+      WALLET: '',
+      KEY: '',
+      RPC: 'https://api.avax.network/ext/bc/C/rpc',
+      FEE_COLLECTOR: '0xAc61FD938E762357eEe739EB30938783366f43a7',
+      ROUTER: '0xE54Ca86531e17Ef3616d22Ca28b0D458b6C89106',
+      SUBGRAPH: 'https://api.thegraph.com/subgraphs/name/pangolindex/exchange',
+      WRAPPED_NATIVE_CURRENCY: '0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7',
+      PNG: '0x60781C2586D68229fde47564546784ab3fACA982',
+      SLIPPAGE_BIPS: 250,
+      MAX_GAS: 7000000,
+      INTERVAL: 48 * 60 * 60 * 1000,
+      INTERVAL_WINDOW: 6 * 60 * 60 * 1000,
+    },
+  },
+];
+
+
+// Post-processing to provide uniqueness
+const path = require('path');
+const ECOSYSTEM = path.basename(__filename, '.config.js');
+const apps = config.map(app => ({
+  ...app,
+  name: `${ECOSYSTEM}/${app.name}`,
+  namespace: ECOSYSTEM,
+}));
+
+module.exports = { apps };
--- src/fee_collector/README.md
+++
@@ -1,9 +0,0 @@
-# Fee Collector
-
-Common tasks for interacting with FeeCollector
-
-### `harvest.js`
-Initiate a PNG buyback using Pangolin protocol's accumulated swap fees. Callers earn a small incentive for doing this. 
-The buyback funds the PNG staking program and the Pangolin treasury. 
-Specify the minimum incentive fee required to execute a buyback in terms of AVAX (default is 0.05 AVAX). 
-Configuration is done internally to the script.
